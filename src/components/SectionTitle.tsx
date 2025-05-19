c5aba7b19c1488f63b6951b4f19c0271a82a77c6
nkien0204 <nkien0204@gmail.com>
2025-05-19 22:37:45 +0700

    build production

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 interface SectionTitleProps {
-    children: React.ReactElement;
+  children: React.ReactElement<{ className: string }>;
 }
 
 const SectionTitle: React.FC<SectionTitleProps> = ({ children }) => {
-    return React.cloneElement(children, {
-        className: children.props.className + " text-3xl lg:text-5xl lg:leading-tight font-bold"
-    });
+  return React.cloneElement(children, {
+    className: " text-3xl lg:text-5xl lg:leading-tight font-bold",
+  });
 };
 
 export default SectionTitle;
